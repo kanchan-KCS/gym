82e63777aeaddd44f467931a94de36a37105ba0f
gourav <gourav@korecent.com>
2023-01-17 11:25:15 +0530

    fix merge

--- gym/api.py
+++ gym/api.py
@@ -6,7 +6,6 @@ def get_stock_qty(item_code):
     try:
         stock_qty = frappe.db.sql(
             """ select actual_qty,warehouse from `tabBin` where item_code="{0}"   """.format(item_code), as_dict=1)
-        
         return stock_qty
     except Exception as stock_error:
         frappe.log_error(frappe.utils.get_traceback(
